2f942d17e61ec412b1a2d6d044e08f3e6d858eaf
blackstate <josemiguelmneyra@gmail.com>
2020-04-30 19:58:53 +0800

    Added initial flask libraries, added initial layout.html template with basic bootstrap navbar, and initial index.html for the starting < / > route

---
+++ application.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ignore cache (remove this after editing css(?))
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# index page 
+@app.route('/')
+def index():
+    test = "Bryle bentalogs badaynedong dantes wewlierevilliame"
+    return render_template("index.html", test=test)
+
